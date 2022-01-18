0ecc0665d5a69c1e428c90fb8753812cd52379e9
SUYEON LEE <82324322+danikaaa@users.noreply.github.com>
2022-01-18 15:29:23 +0900

    Update delete.php

--- delete.php
+++ delete.php
@@ -16,8 +16,8 @@
         <h1><a href="/">자유게시판</a></h1>
         <h4>글을 삭제하기 위해서 비밀번호를 입력해주세요.</h4><hr>
         <form action="delete_ok.php?no=<?php echo $no; ?>" method="post" enctype="multipart/form-data">   
-            <div id = "de_pw"><input type="password" name="pw" id="pw"  placeholder="비밀번호" required /></div><br>
-            <div id = "de_btn"><button type="submit">글 삭제</button></div>
+            <div id = "delete_pw"><input type="password" name="pw" id="pw"  placeholder="비밀번호" required /></div><br>
+            <div id = "delete_btn"><button type="submit">글 삭제</button></div>
         </form>
     
     </body>
