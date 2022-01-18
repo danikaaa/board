f71e42cefa881f5cea5a4edcd7a84292f9ee83af
SUYEON LEE <82324322+danikaaa@users.noreply.github.com>
2022-01-18 15:31:05 +0900

    Update modify_ok.php

--- modify_ok.php
+++ modify_ok.php
@@ -9,11 +9,11 @@ $content = $_POST['content'];
 
 $tmpfile =  $_FILES['file']['tmp_name'];
 $o_name = $_FILES['file']['name'];
-$filename = iconv("UTF-8", "EUC-KR",$_FILES['file']['name']);
-$folder = "upload/".$filename;
-move_uploaded_file($tmpfile,$folder);
+$filename = iconv("UTF-8", "EUC-KR", $_FILES['file']['name']);
+$folder = "upload/" . $filename;
+move_uploaded_file($tmpfile, $folder);
 
-$sql = query("update board set name='".$name."',pw='".$pw."',title='".$title."',content='".$content."', file='".$o_name."' where no='".$no."'"); ?>
+$sql = query("update board set name='" . $name."',pw='" . $pw . "',title='" . $title . "',content='" . $content . "', file='" . $o_name . "' where no='" . $no . "'"); ?>
 
 <script type="text/javascript">alert("수정되었습니다."); </script>
 <meta http-equiv="refresh" content="0 url=read.php?no=<?php echo $no; ?>">
