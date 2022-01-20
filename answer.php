895c90015ef625d5a8a78aa1b1131c00f4999bc8
SUYEON LEE <82324322+danikaaa@users.noreply.github.com>
2022-01-20 17:41:43 +0900

    Update answer.php

--- answer.php
+++ answer.php
@@ -7,9 +7,16 @@ $reply_pw = $_POST['reply_pw'];
 $reply_title = $_POST['reply_title'];
 $reply_content = $_POST['reply_content'];
 $date = date('Y-m-d');
-$orderby = $_POST[''];
 
-$sql = query("insert into board(orderby, depth, name,pw,title,content,date, no) values(orderby+1 , depth+1 ,'" . $reply_name . "', '" . $reply_pw . "', '" . $reply_title . "', '" . $reply_content . "', '" . $date . "')"); 
+$sql = query("SELECT * FROM board WHERE no ='" . $no . "'");
+$reply = mysqli_fetch_array($sql);
+
+$idx = $reply['idx'];
+$orderby = $reply['orderdy']+1;
+$step = $reply['step']+1;
+
+$sql = query("UPDATE board SET orderby = orderby+1 WHERE idx = '$idx' AND orderby > '" . $reply['orderby'] . "' ");
+$sql = query("insert into board(idx, orderby, step, name, pw, title, content, date) values('" . $idx . "','" . $orderby . "','" . $step . "','" . $reply_name . "', '" . $reply_pw . "', '" . $reply_title . "', '" . $reply_content . "', '" . $date . "')"); 
 $sql2 = query("ALTER TABLE board AUTO_INCREMENT=1");
 $sql2 = query("SET @COUNT = 0");
 $sql2 = query("UPDATE board SET no = @COUNT:=@COUNT+1");
